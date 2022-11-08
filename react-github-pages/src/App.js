96f293f57f37ee0e0bea7b61fddcb6d41e93dfe5
Samira <shaikhfiza2018@gmail.com>
2022-11-08 18:45:41 +0530

    added search btn

--- react-github-pages/src/App.js
+++ react-github-pages/src/App.js
@@ -7,6 +7,12 @@ function App() {
       <header className="App-header">
         Cocktails
        <p>Choose your favourite cocktails.</p>
+       <div className="input-group mb-3">
+       <input type="text" className="form-control" placeholder="Search Cocktail" aria-label="Search Cocktail" aria-describedby="basic-addon2"/><span><button type="button" className="btn btn-primary">Primary</button></span>
+       
+       
+      
+      </div>
       </header>
     </div>
   );
