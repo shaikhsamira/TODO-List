733f7b1377c5e23323f38bf3763f3004a7ea0fea
Samira <shaikhfiza2018@gmail.com>
2022-11-24 17:12:39 +0530

    Header added

--- react-github-pages/src/App.js
+++ react-github-pages/src/App.js
@@ -1,20 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import {Header}  from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Cocktails
-       <p>Choose your favourite cocktails.</p>
-       <div className="input-group mb-3">
-       <input type="text" className="form-control" placeholder="Search Cocktail" aria-label="Search Cocktail" aria-describedby="basic-addon2"/><span><button type="button" className="btn btn-primary">Primary</button></span>
-       
-       
-      
-      </div>
-      </header>
-    </div>
+    <Header/>
   );
 }
 
