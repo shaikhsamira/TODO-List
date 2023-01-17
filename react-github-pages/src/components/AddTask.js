cab2e9bd528db76fd13b42cde9feae77846a8e05
Samira <shaikhfiza2018@gmail.com>
2023-01-17 20:10:51 +0530

    Added datetime and some styles

--- react-github-pages/src/components/AddTask.js
+++ react-github-pages/src/components/AddTask.js
@@ -5,10 +5,11 @@ import { useState } from "react";
 
 
 const AddTask = () => {
-    const defaultTask=[{
-        "title":"Default",
-        "status":"new"
-    }]
+    // const defaultTask=[{
+    //     "title":"Default",
+    //     "status":"new",
+    //     "DtTm":"2023"
+    // }]
     const [task, setTask] =useState([])
     const [ userInput, setUserInput ] = useState('');
 
@@ -19,13 +20,17 @@ const AddTask = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        const newTask={
-            "id":task.length+1,
-            "title":userInput,
-            "status":"new"
-          }
+        var currentDate = new Date();
+        var Dt=currentDate.toString().substring(0,15)
+        console.log(currentDate.toString().substring(0,24))
+        // const newTask={
+        //     "id":task.length+1,
+        //     "title":userInput,
+        //     "status":"new",
+        //     "DtTm":Dt
+        //   }
           setTask([...task,{"id":task.length+1,"title":userInput,
-          "status":"new"}])
+          "status":"new","DtTm":Dt}])
     }
 
     const handleDelete = (id) => {
@@ -39,20 +44,30 @@ const AddTask = () => {
       
       
       return (
-        <div className="container">
-            <div className="container h-100 d-flex align-items-center justify-content-center">
-            <div className="form-inline ">
-                <label className="sr-only" for="inlineFormInputName2">AddTask</label>
-                <input type="text" className="form-control mb-2 mr-sm-2" id="inlineFormInputName2"  value={userInput} onChange={(e)=>handleOnChange(e.target.value)} placeholder="Enter task..."/>
+        // <div className="container">
+        //     <div className="container h-100 d-flex align-items-center justify-content-center">
+        //     <div className="form-inline ">
+        //         <label className="sr-only" for="inlineFormInputName2">AddTask</label>
+        //         <input type="text" className="form-control mb-2 mr-sm-2" id="inlineFormInputName2"  value={userInput} onChange={(e)=>handleOnChange(e.target.value)} placeholder="Enter task..."/>
 
-                <button  className="btn btn-primary mb-2" onClick={(e)=>{handleSubmit(e)}} >Add</button>
-            </div>
-            </div>
+        //         <button  className="btn btn-primary mb-2" onClick={(e)=>{handleSubmit(e)}} >Add</button>
+        //     </div>
+        //     </div>
             
-            <div className="container h-100 d-flex align-items-center justify-content-center">
-                <DisplayAllTask taskData={task} handleDelete={handleDelete}/>
-            </div>
-        </div>
+        //     <div className="container h-100 d-flex align-items-center justify-content-center">
+        //         <DisplayAllTask taskData={task} handleDelete={handleDelete}/>
+        //     </div>
+        // </div>
+                    <div class="card  mx-auto" style={{"width": "80%"}}>
+                        <div class="card-header text-center">
+                            <h5 class="card-title" >To Do gives you focus, from work to play. Get started</h5>
+                            <input type="text" className="form-control mb-2 mr-sm-2" id="inlineFormInputName2"  value={userInput} onChange={(e)=>handleOnChange(e.target.value)} placeholder="Enter task..."/>
+                            <button  className="btn btn-primary mb-2" onClick={(e)=>{handleSubmit(e)}} >Add</button>
+                        </div>
+                        <div class="card-body">
+                        <DisplayAllTask taskData={task} handleDelete={handleDelete}/>
+                        </div>
+                     </div>
 
     )
 }
--- react-github-pages/src/components/DisplayAllTask.js
+++ react-github-pages/src/components/DisplayAllTask.js
@@ -12,35 +12,45 @@ export default function DisplayAllTask({taskData,handleDelete}) {
   return (
    
     <div>
-     {
+     
+        <ul class="list-group list-group-flush">
+        {
         taskData.map((i)=>{
             if(i.status=="new"){
                 return (
                     
-                    <div class="row">
-                        <tbody>
+                        
+                        
+                        <li class="list-group-item">
                             <tr>
-                            <th >{i.title}</th>
-                            <td><button type="button" class="btn btn-danger btn-sm" id={i.id} onClick={e=>handleDelete(e.target.id)}>Delete</button></td>
-                            <td><button type="button" class="btn btn-success btn-sm">Complete</button></td>
+                            <td>{i.title}</td>
+                            <td>
+                            <div class="btn-group " role="group" aria-label="Basic outlined example">
+                                <button type="button" class="btn btn-sm float-right btn-outline-dark">{i.DtTm}</button>
+                                <button type="button" class="btn btn-sm float-right btn-outline-warning">edit</button>
+                                <button type="button" class="btn btn-sm float-right  btn-outline-success">delete</button>
+                            </div>
+                            </td>
+                            {/* <td><button type="button" class="btn btn-primary btn-sm float-right"  id={i.id} onClick={e=>handleDelete(e.target.id)}>Delete</button></td> */}
+                            {/* <td><button type="button" class="btn btn-success btn-sm pull-right" style={{"float":"right"}}>Complete</button></td> */}
                             </tr>
                            
-                        </tbody>
-                        </div>
+                        </li>
                 )       
             }
             else{
             return (
-                <table>
+                <li class="list-group-item">
                     <tr>
                         <td style={{"text-decoration":"line-through"}}>{i.title}</td>
                     </tr>
-                </table>
+                </li>
             )       
         }           
             
         })
      }
+     </ul>
     </div>
   );
 }
